7c7f32b3dd57f0825f4720e5e1b1f4dbecdd9165
Karthikgowda143 <karthiknatrajgowda@gmail.com>
2025-08-11 19:15:20 +0530

    11

---
+++ include/local_storage_handler.h
@@ -0,0 +1,24 @@
+#ifndef LOCAL_STORAGE_HANDLER_H
+#define LOCAL_STORAGE_HANDLER_H
+
+#include "main.h"
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+
+// -------------------- CONFIG --------------------     
+#define LOG_FOLDER    "/logs"
+#define MAX_LOG_DAYS  30
+#define LOG_INTERVAL  60000    // 5 min for periodic logs (example)
+
+#define SD_MISO   13
+#define SD_MOSI   11
+#define SD_SCK    12
+#define SD_CS     10
+
+
+void setupLocalStorage();
+void writeLog(String entry);
+void logData(String logType, String eventType, String description);
+
+#endif // LOCAL_STORAGE_HANDLER_H
